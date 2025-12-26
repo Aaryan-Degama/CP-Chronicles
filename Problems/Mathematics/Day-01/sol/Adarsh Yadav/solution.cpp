93d3128cbe9fcf5ceafa0b5c97901af0ec0b8676
adrsh2808-creator <adrsh2808@gmail.com>
2025-12-26 23:33:49 +0530

    Create solution.cpp

---
+++ Problems/Mathematics/Day-01/sol/Adarsh Yadav/solution.cpp
@@ -0,0 +1,26 @@
+//https://codeforces.com/contest/1560/problem/C
+// TC : O(1) SC: O(1)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    int k=abs(a-b);
+	    if(a>2*k || b>2*k || c>2*k){
+	        cout<<-1<<endl;
+	    }
+	    else{
+	        if((c-k)>0){
+	            cout<<c-k<<endl;
+	        }
+	        else{
+	            cout<<c+k<<endl;
+	        }
+	    }
+	}
+
+}
